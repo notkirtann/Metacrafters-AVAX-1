8f6be5388028971a09728715b43bed0c89b190a7
Kirtan Nahar <119801478+notkirtann@users.noreply.github.com>
2024-06-15 23:26:01 +0530

    Create Error-Handling-Assesment.sol

---
+++ Error-Handling-Assesment.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.18;
+
+contract ErrorHandlingAssesment{
+
+    uint public count=0;
+
+    function testingRequire_In_Solidity(uint money) public{
+        require(money > 1000,"Entered Amount must be greater than 1000");
+        count++;
+    }
+
+    function testingRevert_In_Solidity(uint money) public {
+        if(!(money > 1000)){
+            revert("Entered Amount must be greater than 1000");
+        }
+        count++;
+    }
+
+    function testingAssert_In_Solidity(uint money) public{
+        assert(money > 1000);
+        count++;
+    }
+
+    error CustomError(uint money, string message);
+
+    function testingCustomError(uint money) public{
+        if(money <= 1000){
+            revert CustomError(money,"Entered Amount must be greater than 1000");
+        }
+        count++;
+    }
+}
